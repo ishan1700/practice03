b12378f3bc9af23005a12cdc51be539701afad22
Ishan Gurao <85431007+ishan1700@users.noreply.github.com>
2024-09-13 05:21:10 +0000

    minor change

--- terraformVPC.tf
+++ terraformVPC.tf
@@ -13,6 +13,7 @@ resource "aws_vpc" "healthApp" {
 resource "aws_subnet" "publicSn" {
   vpc_id     = aws_vpc.healthApp.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch="true"
 
   tags = {
     Name = "publicSn"
