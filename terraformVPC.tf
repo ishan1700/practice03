7159b650af968a4e42fbd82fa835613384849608
Ishan Gurao <85431007+ishan1700@users.noreply.github.com>
2024-09-13 05:20:04 +0000

    Subnets

--- terraformVPC.tf
+++ terraformVPC.tf
@@ -7,3 +7,24 @@ resource "aws_vpc" "healthApp" {
     Name = "healthApp"
   }
 }
+
+#Subnet
+
+resource "aws_subnet" "publicSn" {
+  vpc_id     = aws_vpc.healthApp.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "publicSn"
+  }
+}
+
+resource "aws_subnet" "privateSn" {
+  vpc_id     = aws_vpc.healthApp.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "privateSn"
+  }
+}
+
